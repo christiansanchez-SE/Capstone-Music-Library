a4262e0c1d7df3b3102bfbf3142d53e4306d151b
christiansanchez-SE <christiansanchez714@gmail.com>
2024-05-23 14:25:40 -0400

    Connected backend and frontend

--- frontend/porject_library/src/App.jsx
+++ frontend/porject_library/src/App.jsx
@@ -1,15 +1,28 @@
-// import { useState } from 'react'
-
-import './styles/App.css'
+import { useState, useEffect } from "react";
+import './styles/App.css';
+import {getMusic} from "./utility/music-api";
 
 function App() {
+  const [musicLibrary, setMusicLibrary] = useState([]);
 
+  useEffect(() => {
+    getMusic(setMusicLibrary);
+  }, []);
 
   return (
-    <div className='app'>
-      App
+    <div className="app">
+      <h1>Music Library</h1>
+      <ul>
+        {musicLibrary.map((music, index) => (
+          <li key={index}>
+            <h2>{music.title}</h2>
+            <p>Artist: {music.artist}</p>
+            <p>Genre: {music.genre.join(", ")}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/porject_library/src/utility/music-api.jsx
@@ -0,0 +1,11 @@
+export async function getMusic(setState) {
+    try {
+      const response = await fetch('http://localhost:3000/musicLibrary');
+      const data = await response.json();
+      setState(data.music);
+      console.log(data.music);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
